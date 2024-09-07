506964c6913915547507df96b1df75dd106d2faa
Keith Bloom <keith.bloom@gmail.com>
2024-09-07 15:14:19 +0100

    adds tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './tournament/templates/**/*.html',
+    './tournament/static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
